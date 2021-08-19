b365c2fbd1ef5c2b7869d98dfaa087c2679e1c36
jasonfangmagic <jasonfangmagic@gmail.com>
2021-08-19 19:08:09 -0400

    Java

---
+++ JavaTest/src/maps.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class maps {
+    public static void main(String[] args) {
+        Map m= new HashMap();
+
+        m.put("Jason", 5);
+
+        System.out.println(m);
+    }
+}
