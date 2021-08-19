d60d965d39282e46d4d02c93f71c8f7a8b8b0f8f
Jason Fang <jasonfangmagic@gmail.com>
2021-08-19 15:03:01 -0400

    Java

---
+++ JavaTest/src/arrays.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class arrays {
+    public static void main(String[] args) {
+
+        String[] newArr = new String [5];
+        newArr[0] = "hello";
+        String x = newArr[0];
+
+        int[] nums = {2,3,4,5,6,7,};
+
+        int y = nums[2];
+
+        System.out.println(y);
+    }
+}
