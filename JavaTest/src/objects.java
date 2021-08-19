77409e5539367f746110961631131f6ce4d6fdf2
jasonfangmagic <jasonfangmagic@gmail.com>
2021-08-19 19:20:29 -0400

    Java

---
+++ JavaTest/src/objects.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class objects {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        sc.next();
+        int x = 5;
+        jason("Jason Fang");
+    }
+    public static void jason(String str) {
+        System.out.println(str);
+    }
+}
