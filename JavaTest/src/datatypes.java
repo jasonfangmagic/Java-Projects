5e74073d15f89086453b079a82670abe6fc46273
Jason Fang <jasonfangmagic@gmail.com>
2021-08-19 10:49:15 -0400

    Java

--- JavaTest/src/datatypes.java
+++ JavaTest/src/datatypes.java
@@ -1,6 +1,12 @@
 public class datatypes {
     public static void main(String[] args) {
+        //primitive data types//
         int hello = 5;
-        System.out.println(hello);
+        double world=5.2;
+        boolean hi = true;
+        char u ='9';
+        //Not primitive//
+        String str = "Hello World!";
+        System.out.println(str);
     }
 }
