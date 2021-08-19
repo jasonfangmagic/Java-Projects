5dbf366e4baec23a0059c8a675ef4260c432354e
Jason Fang <jasonfangmagic@gmail.com>
2021-08-19 11:12:08 -0400

    Java

---
+++ JavaTest/src/operators.java
@@ -0,0 +1,12 @@
+public class operators {
+    public static void main(String[] args) {
+    double x ,y;
+    x=6;
+    y=7;
+    double sum = x / y;
+    double d = Math.pow(x,y);
+    System.out.println(x);
+    System.out.println(sum);
+    System.out.println(d);
+    }
+}
