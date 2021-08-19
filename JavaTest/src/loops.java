a85527cdf9817ca8c9802b1b15bebf3676429d2f
Jason Fang <jasonfangmagic@gmail.com>
2021-08-19 15:13:42 -0400

    Java

--- JavaTest/src/loops.java
+++ JavaTest/src/loops.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args) {
         int x =0;
-        for (int i = 0; i <=10; i++) {
+        int[] arr = {2,3,4,5,6,7,8};
+        for (int i = 0; i <= arr.length; i++) {
             System.out.println(i);
+            System.out.println(arr[0]);
         }
     }
 }
