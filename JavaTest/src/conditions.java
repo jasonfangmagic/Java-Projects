373a12f7fcc97ac0cfdda71fc1e2c84814055498
Jason Fang <jasonfangmagic@gmail.com>
2021-08-19 12:04:03 -0400

    Java

---
+++ JavaTest/src/conditions.java
@@ -0,0 +1,11 @@
+public class conditions {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 7;
+        int z =10;
+
+        // > < == >= <= !=//
+        boolean compare;
+        System.out.println();
+    }
+}
