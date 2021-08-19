8800ad21e59078ff69945fec843327c2a97df5e7
Jason Fang <jasonfangmagic@gmail.com>
2021-08-19 12:23:26 -0400

    Java

---
+++ JavaTest/src/ifelse.java
@@ -0,0 +1,12 @@
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+
+public class ifelse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+    if (s.equals("Jason")) {
+        System.out.println("You typed Jason");
+    }
+    }
+}
